d0441af590687eafee8915e4ac6464108377cb7f
y2code dev <y2codedev@gmail.com>
2024-12-18 13:00:30 +0530

    fix issues

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,5 @@
 import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from "uuid";
 export const showToast = (type, message) => {
     switch (type) {
         case 'success':
@@ -29,3 +30,11 @@ export const showToast = (type, message) => {
     }
 };
 
+export const getSessionId = () => {
+  let sessionId = localStorage.getItem("session_id");
+  if (!sessionId) {
+    sessionId = uuidv4();
+    localStorage.setItem("session_id", sessionId);
+  }
+  return sessionId;
+};
